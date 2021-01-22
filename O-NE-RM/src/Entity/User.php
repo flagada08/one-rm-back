455c4cb4d2eff6b84f84ee698a9b2ed35f7f9bb1
Chaliebauduin <bauduincharlie@gmail.com>
2021-01-22 11:15:07 +0100

    BDD refaite et fonctionnalité create User OK

--- O-NE-RM/src/Entity/User.php
+++ O-NE-RM/src/Entity/User.php
@@ -2,85 +2,69 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
-     * @Groups("test")
-     * 
      */
     private $id;
 
     /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
      * @ORM\Column(type="string", length=10)
-     * @Groups("test")
-     * @Groups("infos")
      */
     private $gender;
 
     /**
      * @ORM\Column(type="string", length=50)
-     * @Groups("test")
-     * @Groups("infos")
      */
     private $firstname;
 
     /**
-     * @ORM\Column(type="string", length=80)
-     * @Groups("test")
-     * @Groups("infos")
+     * @ORM\Column(type="string", length=70)
      */
     private $lastname;
 
     /**
      * @ORM\Column(type="integer")
-     * @Groups("test")
-     * @Groups("infos")
      */
     private $age;
 
     /**
-     * @ORM\Column(type="string")
-     * @Groups("infos")
-     * 
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     * 
-     */
-    private $password;
-
-    /**
-     * @ORM\Column(type="string", length=60, nullable=true)
-     * @Groups("infos")
-     */
-    private $role;
-
-    /**
      * @ORM\ManyToOne(targetEntity=FitnessRoom::class, inversedBy="users")
-     * @Groups("test")
-     * @Groups("infos")
      */
     private $fitnessRoom;
 
     /**
-     * @ORM\OneToMany(targetEntity=Goal::class, mappedBy="user", orphanRemoval=true)
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="user", orphanRemoval=true)
      */
-    private $goals;
+    private $comments;
 
     /**
      * @ORM\OneToMany(targetEntity=Progress::class, mappedBy="user", orphanRemoval=true)
@@ -88,20 +72,93 @@ class User
     private $progress;
 
     /**
-     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="user", orphanRemoval=true)
+     * @ORM\OneToMany(targetEntity=Goal::class, mappedBy="user", orphanRemoval=true)
      */
-    private $comments;
+    private $goals;
 
     public function __construct()
     {
-        $this->goals = new ArrayCollection();
-        $this->progress = new ArrayCollection();
         $this->comments = new ArrayCollection();
+        $this->progress = new ArrayCollection();
+        $this->goals = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getGender(): ?string
@@ -152,42 +209,6 @@ class User
         return $this;
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
-
-    public function setRole(?string $role): self
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-
     public function getFitnessRoom(): ?FitnessRoom
     {
         return $this->fitnessRoom;
@@ -201,29 +222,29 @@ class User
     }
 
     /**
-     * @return Collection|Goal[]
+     * @return Collection|Comment[]
      */
-    public function getGoals(): Collection
+    public function getComments(): Collection
     {
-        return $this->goals;
+        return $this->comments;
     }
 
-    public function addGoal(Goal $goal): self
+    public function addComment(Comment $comment): self
     {
-        if (!$this->goals->contains($goal)) {
-            $this->goals[] = $goal;
-            $goal->setUser($this);
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setUser($this);
         }
 
         return $this;
     }
 
-    public function removeGoal(Goal $goal): self
+    public function removeComment(Comment $comment): self
     {
-        if ($this->goals->removeElement($goal)) {
+        if ($this->comments->removeElement($comment)) {
             // set the owning side to null (unless already changed)
-            if ($goal->getUser() === $this) {
-                $goal->setUser(null);
+            if ($comment->getUser() === $this) {
+                $comment->setUser(null);
             }
         }
 
@@ -261,29 +282,29 @@ class User
     }
 
     /**
-     * @return Collection|Comment[]
+     * @return Collection|Goal[]
      */
-    public function getComments(): Collection
+    public function getGoals(): Collection
     {
-        return $this->comments;
+        return $this->goals;
     }
 
-    public function addComment(Comment $comment): self
+    public function addGoal(Goal $goal): self
     {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setUser($this);
+        if (!$this->goals->contains($goal)) {
+            $this->goals[] = $goal;
+            $goal->setUser($this);
         }
 
         return $this;
     }
 
-    public function removeComment(Comment $comment): self
+    public function removeGoal(Goal $goal): self
     {
-        if ($this->comments->removeElement($comment)) {
+        if ($this->goals->removeElement($goal)) {
             // set the owning side to null (unless already changed)
-            if ($comment->getUser() === $this) {
-                $comment->setUser(null);
+            if ($goal->getUser() === $this) {
+                $goal->setUser(null);
             }
         }
 
