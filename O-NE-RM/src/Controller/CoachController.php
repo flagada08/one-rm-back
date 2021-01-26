2f3682e83c2ac7346e1329b175e61f343efacffe
Chaliebauduin <bauduincharlie@gmail.com>
2021-01-26 11:48:52 +0100

    ajout du coachController

--- O-NE-RM/src/Controller/CoachController.php
+++ O-NE-RM/src/Controller/CoachController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class CoachController extends AbstractController
 {
     /**
-     * @Route("/api/admin", name="admin")
+     * @Route("/coach", name="coach")
      */
     public function index(): Response
     {
-        
+        return $this->render('coach/index.html.twig', [
+            'controller_name' => 'CoachController',
+        ]);
     }
 }
