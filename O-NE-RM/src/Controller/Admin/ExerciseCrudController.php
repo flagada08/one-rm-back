730e40f33a2d8d4909b7249c9835650ab2b241a7
Chaliebauduin <bauduincharlie@gmail.com>
2021-02-06 18:15:48 +0100

    modif du dashboard

--- O-NE-RM/src/Controller/Admin/ExerciseCrudController.php
+++ O-NE-RM/src/Controller/Admin/ExerciseCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Exercise;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ExerciseCrudController extends AbstractCrudController
@@ -12,14 +15,16 @@ class ExerciseCrudController extends AbstractCrudController
         return Exercise::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            NumberField::new('difficulty'),
+            TextField::new('illustration'),
+            TextField::new('advice')
         ];
     }
-    */
+    
 }
